79fb422c99749325574e4171f4c806b33d565e04
jeanjun <jjeanjun0@gmail.com>
2024-06-10 22:22:56 +0900

    feat(cli): embedMuPDF

--- cli/src/commands/embedMuPDF.ts
+++ cli/src/commands/embedMuPDF.ts
@@ -1,3 +1,22 @@
-export const embedMuPDF = () => {
-  
+import fs from 'fs'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+type Options = {
+  path: string
+}
+
+export const embedMuPDF = async ({
+  path: targetPath
+}: Options) => {
+  const currentDir = dirname(fileURLToPath(import.meta.url))
+  const source = join(currentDir, '../node_modules/mupdf/dist')
+
+  const files = await fs.promises.readdir(source)
+  for (const file of files) {
+    const srcPath = join(source, file)
+    const destPath = join(targetPath, file)
+
+    await fs.promises.copyFile(srcPath, destPath)
+  }
 }
--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist'
-import { commands } from './commands'
+import { program } from 'commander'
+import { embedMuPDF } from './commands/embedMuPDF'
 
-;(async () => {
-  const args = minimist(process.argv.slice(2))
-  const { _, ...options } = args
-  
-  const command = commands[args._[0] as keyof typeof commands]
-  if (typeof command === 'function') {
+program
+  .command('embed-mupdf')
+  .description('MuPDF.js의 빌드된 버전을 원하는 경로에 배포합니다. @onipdf/core 패키지에서 사용합니다.')
+  .option('-p, --path <path>', '복사할 폴더의 경로를 지정해주세요')
+  .action(async (options) => {
     try {
-      await command(options)
+      await embedMuPDF(options)
+      console.log('파일이 성공적으로 복사 되었습니다.')
     } catch (error) {
+      console.error('파일 복사 중 오류가 발생했습니다:', error)
       process.exit(1)
     }
-  }
-})()
+  })
+
+program.parse(process.argv)
