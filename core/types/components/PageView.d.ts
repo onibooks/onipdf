12e3e1eee3f2de35755e819f871ae87b3dea0ba5
momogoyo <metheduxk@gmail.com>
2024-11-20 14:19:43 +0900

    🚧

--- core/types/components/PageView.d.ts
+++ core/types/components/PageView.d.ts
@@ -1,4 +1,5 @@
 import type { GlobalContext } from '../provider';
+<<<<<<< Updated upstream
 type PageViewProps = {
     context: GlobalContext;
     pageIndex: number;
@@ -7,4 +8,17 @@ type PageViewProps = {
 declare const PageView: import("preact").FunctionalComponent<import("preact/compat").PropsWithoutRef<PageViewProps> & {
     ref?: import("preact").Ref<unknown> | undefined;
 }>;
+=======
+type Size = {
+    width: number;
+    height: number;
+};
+type PageViewProps = {
+    context: GlobalContext;
+    pageSize: Size;
+    pageIndex: number;
+    pageRender: (value: unknown) => void;
+};
+declare const PageView: ({ context, pageSize, pageIndex, pageRender }: PageViewProps) => import("preact/compat").JSX.Element;
+>>>>>>> Stashed changes
 export default PageView;
--- core/types/components/Spread/Single.d.ts
+++ core/types/components/Spread/Single.d.ts
@@ -1,8 +1,13 @@
 import type { GlobalContext } from '../../provider';
 type SingleProps = {
     context: GlobalContext;
+<<<<<<< Updated upstream
     observer: IntersectionObserver | null;
     pageViewRefs: any;
 };
 declare const Single: ({ context, observer, pageViewRefs }: SingleProps) => import("preact").JSX.Element;
+=======
+};
+declare const Single: ({ context }: SingleProps) => import("preact").JSX.Element;
+>>>>>>> Stashed changes
 export default Single;
